9d75df710f1fa908792323346aeaf6412a86a76f
kajarenc <kajarenc@gmail.com>
2021-12-14 02:24:19 +0400

    mobile only camera switch

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Webcam image input demo app!")
+
+image_file = st.camera_input("LABEL", help="Help Help")
+
+if image_file is not None:
+    x = st.slider("ROTATE BY X DEGREE", 0, 360, 0)
+
+    pil_image = Image.open(image_file)
+    rotated_image = pil_image.rotate(x)
+    st.image(rotated_image)
